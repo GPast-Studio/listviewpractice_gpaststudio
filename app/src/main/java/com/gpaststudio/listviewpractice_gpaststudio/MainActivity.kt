934057d18f783d65de71da84ceb9e2b53b72ccf1
GPast-Studio <gdeenk@gmail.com>
2022-10-11 17:23:58 +0900

    학생 목록을 담을 arraylist를 생성하고 실제 목록 추가

--- app/src/main/java/com/gpaststudio/listviewpractice_gpaststudio/MainActivity.kt
+++ app/src/main/java/com/gpaststudio/listviewpractice_gpaststudio/MainActivity.kt
@@ -2,10 +2,24 @@ package com.gpaststudio.listviewpractice_gpaststudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gpaststudio.listviewpractice_gpaststudio.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+    val mStuduentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStuduentList.add( Student("GP-Studio",1989) )
+        mStuduentList.add( Student("박선진",1989) )
+        mStuduentList.add( Student("이정민",1997) )
+        mStuduentList.add( Student("김영례",1961) )
+        mStuduentList.add( Student("이주현",1964) )
+
+
+
+
     }
 }
