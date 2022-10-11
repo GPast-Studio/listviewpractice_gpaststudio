ddccc7e9b03e64a1b297d6e11076e782b2db7af8
GPast-Studio <gdeenk@gmail.com>
2022-10-11 17:33:52 +0900

    어뎁터 변수를 추가, 리스트뷰의 어뎁터로서의 연결

--- app/src/main/java/com/gpaststudio/listviewpractice_gpaststudio/MainActivity.kt
+++ app/src/main/java/com/gpaststudio/listviewpractice_gpaststudio/MainActivity.kt
@@ -2,11 +2,15 @@ package com.gpaststudio.listviewpractice_gpaststudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gpaststudio.listviewpractice_gpaststudio.adapters.StudentAdapter
 import com.gpaststudio.listviewpractice_gpaststudio.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mStuduentList = ArrayList<Student>()
+
+    lateinit var madapter : StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,12 @@ class MainActivity : AppCompatActivity() {
         mStuduentList.add( Student("이정민",1997) )
         mStuduentList.add( Student("김영례",1961) )
         mStuduentList.add( Student("이주현",1964) )
+
+
+        madapter = StudentAdapter(this, R.layout.student_list_item,mStuduentList)
+
+
+        studentListView.adapter = madapter
 
 
 
