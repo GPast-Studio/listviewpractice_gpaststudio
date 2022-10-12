811f2ef6aa6bbdc5d8a397c5c54102e174b7427a
GPast-Studio <gdeenk@gmail.com>
2022-10-13 05:36:29 +0900

    학생 클래스 기능 추가 - 넣는 숫자 값에 따라 달리 로그를 찍는 기능

--- app/src/main/java/com/gpaststudio/listviewpractice_gpaststudio/datas/Student.kt
+++ app/src/main/java/com/gpaststudio/listviewpractice_gpaststudio/datas/Student.kt
@@ -1,8 +1,36 @@
 package com.gpaststudio.listviewpractice_gpaststudio.datas
 
+import android.util.Log
+
 // 학생의 하위 정보 - 이름, 출생연월
 
 class Student (
     val name : String,
     val birthYear : Int) {
+
+//    학생의 기능? 예시
+//    자신의 이름을 로그로 찍는 기능
+
+        fun printMyNameToLog(Num1 : Int, Num2 : Double ) : Boolean {
+
+//            num1이 num2보다 크면 로그를 이름만
+//            그렇지않다면, 이름과 인사도 로그 출력
+
+        if(Num1 > Num2) {
+
+            Log.d("연습용",this.name)
+        }
+
+            else {
+                Log.d("연습용", this.name)
+                Log.d("연습용","잘 부탁드립니다")
+            }
+
+            return true
+
+        }
+
+
+
+
 }
